144bfcb30c56b910b8fc6b3fe383f673df4628b4
Aktan Kerimbekov <kerimbekov1233@gmail.com>
2024-04-07 11:11:57 +0600

    add map

--- lib/presentation/widgets/company_list.dart
+++ lib/presentation/widgets/company_list.dart
@@ -8,31 +8,33 @@ class CompanyList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    Map<String, String> companyInfo = {
-      "Apple": "assets/png/apple_logo.png",
-      "Concept2": "assets/png/c2_logo.png",
-      "Eight": "assets/png/eight_logo.png",
-      "FitBit": "assets/png/fitbit_logo.png",
-      "Garmin": "assets/png/garmig_logo.png",
-      "Google Fit": "assets/png/googlefit_logo.png",
-      "Google": "assets/png/googlefit_logo.png",
-      "Goog": "assets/png/googlefit_logo.png",
-    
-    };
+    List<CompanyInfo> companyInfo = [
+      CompanyInfo(text: "Apple", image: "assets/png/apple_logo.png"),
+      CompanyInfo(text: "Concept2", image: "assets/png/c2_logo.png"),
+      CompanyInfo(text: "Eight", image: "assets/png/eight_logo.png"),
+      CompanyInfo(text: "FitBit", image: "assets/png/fitbit_logo.png"),
+      CompanyInfo(text: "Garmin", image: "assets/png/garmig_logo.png"),
+      CompanyInfo(text: "Google Fit", image: "assets/png/googlefit_logo.png"),
+      CompanyInfo(text: "Google", image: "assets/png/googlefit_logo.png"),
+      CompanyInfo(text: "Goog", image: "assets/png/googlefit_logo.png"),
+    ];
     return Padding(
       padding: const EdgeInsets.all(24.0),
       child: Column(
-        children: [
-          ...List.generate(
-              companyInfo.length,
-              (index) => GestureDetector(
-                    child: CompanyCard(
-                      companyText: companyInfo.keys.elementAt(index),
-                      imageUrl: companyInfo.values.elementAt(index),
-                    ),
-                  ))
-        ],
-      ),
+          children: companyInfo
+              .map(
+                (e) => CompanyCard(
+                  companyText: e.text,
+                  imageUrl: e.image,
+                ),
+              )
+              .toList()),
     );
   }
+}
+
+class CompanyInfo {
+  CompanyInfo({required this.text, required this.image});
+  final String text;
+  final String image;
 }
